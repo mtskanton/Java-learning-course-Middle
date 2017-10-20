44aaada6abaa4a04e31db26bb7fc48356d251d4c
mtskanton <mtsk.anton@yandex.ru>
2017-10-20 17:42:14 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j;
 
+/**
+* Calculate. Вывод надписи Hello World
+*
+* @author Anton Matsik (mailto:mtsk_anton@yandex.ru)
+* @since 20.10.2017
+*/
 public class Calculate {
+	
+	/**
+	* Метод запуска программы
+	* Вывод строки в консоль
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}		
