09df9961a14a0c3abe2a58a0aefcbb19e7998158
mtskanton <mtsk.anton@yandex.ru>
2017-10-21 20:07:31 +0300

    3.3. Вычисление площади треугольника [9461]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,78 @@
+package ru.job4j.condition;
+
+/**
+ * Вычисление площади треугольника.
+ * @author Anton Matsik
+ * @since 21.10.2017
+ */
+public class Triangle {
+    /**
+     * Объявление переменных класса Point.
+     */
+    private Point a, b, c;
+
+    /**
+     * Конструктор инициализации значений.
+     * @param a точка а
+     * @param b точка b
+     * @param c точка c
+     */
+    public Triangle(Point a, Point b, Point c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    /**
+     * Вычисление расстояние между точками left и right.
+     * √(xb - xa)^2 + (yb - ya)^2
+     * @param left Координаты точки слева
+     * @param right Координаты точки справа
+     * @return расстояние между точками left и right
+     */
+    public double distance(Point left, Point right) {
+        double leftX = left.getX();
+        double leftY = left.getY();
+        double rightX = right.getX();
+        double rightY = right.getY();
+        return Math.sqrt(Math.pow((rightX - leftX), 2) + Math.pow((rightY - leftY), 2));
+    }
+
+    /**
+     * Вычисление периметра по длинам сторон.
+     * @param ab расстояние между точками a b
+     * @param ac расстояние между точками a c
+     * @param bc расстояние между точками b c
+     * @return Перимент.
+     */
+    public double period(double ab, double ac, double bc) {
+        return (ab + ac + bc) / 2;
+    }
+
+    /**
+     * Вычисление прощади треуголька по формуле Герона.
+     * @return Вернуть прощадь, если треугольник существует или -1
+     */
+    public double area() {
+        double rsl = -1;
+        double ab = this.distance(this.a, this.b);
+        double ac = this.distance(this.a, this.c);
+        double bc = this.distance(this.b, this.c);
+        double p = this.period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            return Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
+        }
+        return rsl;
+    }
+
+    /**
+     * Проверка, можно ли построить треугольник с такими длинами сторон.
+     * @param ab Длина от точки a b
+     * @param ac Длина от точки a c
+     * @param bc Длина от точки b c
+     * @return true если все стороны больше нуля
+     */
+    private boolean exist(double ab, double ac, double bc) {
+        return (ab > 0 && ac > 0 && bc > 0);
+    }
+}
