3b393560dfd4118ae2984ae2def1c473eef1a4a2
mtskanton <mtsk.anton@yandex.ru>
2017-11-14 18:49:28 +0300

    Создать итератор четные числа [#150] / Совмещены методы next() и hasNext()

--- chapter_005/src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
+++ chapter_005/src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -5,34 +5,13 @@ import java.util.NoSuchElementException;
 
 public class EvenNumbersIterator implements Iterator {
     private final int[] array;
-    int index = 0;
+    private int index = 0;
 
     EvenNumbersIterator(int[] array) {
         this.array = array;
     }
 
     public boolean hasNext() {
-        return  evenExist();
-    }
-
-    public Object next() {
-        if (!evenExist()) {
-            throw new NoSuchElementException();
-        } else {
-            int result = 0;
-            for (int i = index; i < array.length; i++) {
-                if (array[i] % 2 == 0) {
-                    result = array[i];
-                    index = i;
-                    break;
-                }
-            }
-            index++;
-            return result;
-        }
-    }
-
-    private boolean evenExist() {
         boolean result = false;
         for (int i = index; i < array.length; i++) {
             if (array[i] % 2 == 0) {
@@ -41,5 +20,19 @@ public class EvenNumbersIterator implements Iterator {
             }
         }
         return result;
+    }
+
+    public Object next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        } else {
+            for (int i = index; i < array.length; i++) {
+                if (array[i] % 2 == 0) {
+                    index = i;
+                    break;
+                }
+            }
+            return array[index++];
+        }
     }
 }
