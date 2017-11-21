5810d9c51d83a23801664aca2ac43b1a3375e342
mtskanton <mtsk.anton@yandex.ru>
2017-11-21 20:33:38 +0300

    Реализовать коллекцию Set на массиве / Исправлено: массив увеличивается в 2 раза при заполнении, поиск дубликата вынесен в отдельный метод

--- chapter_005/src/main/java/ru/job4j/set/SimpleSet.java
+++ chapter_005/src/main/java/ru/job4j/set/SimpleSet.java
@@ -5,29 +5,31 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class SimpleSet<E> implements Iterable<E> {
-    private Object[] container = new Object[0];
+    private Object[] container = new Object[1];
     int size = 0;
-    int index = 0;
 
     public void add(E value) {
-        boolean duplicate = false;
+        if (!duplicate(value)) {
+            if (container.length == size) {
+                container = Arrays.copyOf(container, size * 2);
+            }
+            container[size++] = value;
+        }
+    }
+
+    private boolean duplicate(E v) {
         for (int i = 0; i < size; i++) {
-            if (container[i].equals(value)) {
-                duplicate = true;
-                break;
+            if (container[i].equals(v)) {
+                return true;
             }
         }
-
-        if (!duplicate) {
-            Object[] array = Arrays.copyOf(container, size + 1);
-            array[size++] = value;
-            container = array;
-        }
+        return false;
     }
 
     @Override
     public Iterator<E> iterator() {
         return new Iterator<E>() {
+            private int index = 0;
 
             @Override
             public boolean hasNext() {
