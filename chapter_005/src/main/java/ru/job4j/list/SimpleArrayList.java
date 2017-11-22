f86344389e586d4d6a93265c660996c2e72e1090
mtskanton <mtsk.anton@yandex.ru>
2017-11-22 20:52:24 +0300

    Создать динамический список / Добавлено исключение в методе next()

--- chapter_005/src/main/java/ru/job4j/list/SimpleArrayList.java
+++ chapter_005/src/main/java/ru/job4j/list/SimpleArrayList.java
@@ -2,11 +2,11 @@ package ru.job4j.list;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class SimpleArrayList<T> implements Iterable<T> {
 
     private int size = 0;
-    private int index = 0;
     private Object[] container = new Object[10];
 
     public void add(T value) {
@@ -23,13 +23,16 @@ public class SimpleArrayList<T> implements Iterable<T> {
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
-
+            private int index = 0;
             public boolean hasNext() {
                 return index < size;
             }
 
             public T next() {
-                return (T) container[index++];
+                if (this.hasNext()) {
+                    return (T) container[index++];
+                }
+                throw new NoSuchElementException();
             }
         };
     }
