30b8e889d3e7f0b8607eb34e59c502cfbcf9ed2b
mtskanton <mtsk.anton@yandex.ru>
2018-01-06 17:59:18 +0300

    Crud servlet / Рефакторинг: Использованы try-with-resources

--- chapter_009/src/main/java/crudservlet/UsersStore.java
+++ chapter_009/src/main/java/crudservlet/UsersStore.java
@@ -11,7 +11,7 @@ import java.util.List;
  */
 public class UsersStore {
     private static final UsersStore INSTANCE = new UsersStore();
-    private static Connection conn;
+    private Connection conn;
 
      private UsersStore() {
          //подключение к БД
@@ -21,7 +21,7 @@ public class UsersStore {
              String user = "postgres";
              String pass = "password";
 
-             conn = DriverManager.getConnection(url, user, pass);
+             this.conn = DriverManager.getConnection(url, user, pass);
          } catch (SQLException | ClassNotFoundException e) {
              e.printStackTrace();
          }
@@ -34,16 +34,13 @@ public class UsersStore {
     /**
      * Метод получения списка пользователей.
      * @return список пользователей
-     * @throws SQLException
+     * @throws SQLException в случае возникновения при SQL запросе
      */
      public List<User> getUsers() throws SQLException {
          List<User> users = new ArrayList<>();
 
-         Statement st = null;
-         ResultSet rs = null;
-         try {
-             st = conn.createStatement();
-             rs = st.executeQuery("SELECT * FROM users");
+         try (Statement st = this.conn.createStatement();
+              ResultSet rs = st.executeQuery("SELECT * FROM users")) {
 
              while (rs.next()) {
                  User user = new User();
@@ -54,16 +51,7 @@ public class UsersStore {
 
                  users.add(user);
              }
-         } finally {
-             if (rs != null) {
-                 rs.close();
-             }
-
-             if (st != null) {
-                 st.close();
-             }
          }
-
          return users;
      }
 
@@ -72,21 +60,18 @@ public class UsersStore {
      * @param name имя
      * @param login логин
      * @param email e-mail
-     * @throws SQLException
+     * @throws SQLException в случае возникновения при SQL запросе
      */
      public void addUser(String name, String login, String email) throws SQLException {
-         PreparedStatement pst = null;
-         try {
-
-
-             pst = conn.prepareStatement("CREATE TABLE IF NOT EXISTS users ("
+         try (PreparedStatement pst = this.conn.prepareStatement("CREATE TABLE IF NOT EXISTS users ("
                              + "id SERIAL PRIMARY KEY,"
                              + "name varchar(255) NOT NULL,"
                              + "login varchar(255) NOT NULL,"
                              + "email varchar(255) NOT NULL,"
                              + "created date DEFAULT now()"
                              + ");"
-                     + "INSERT INTO users (name, login, email, created) VALUES (?, ?, ?, ?)");
+                     + "INSERT INTO users (name, login, email, created) VALUES (?, ?, ?, ?)")) {
+
              pst.setString(1, name);
              pst.setString(2, login);
              pst.setString(3, email);
@@ -94,11 +79,6 @@ public class UsersStore {
              Date dataTime = new java.sql.Date(d.getTime());
              pst.setDate(4, dataTime);
              pst.executeUpdate();
-
-         } finally {
-             if (pst !=  null) {
-                pst.close();
-             }
          }
      }
 
@@ -108,41 +88,27 @@ public class UsersStore {
      * @param name новое имя
      * @param login новый логин
      * @param email новый e-mail
-     * @throws SQLException
+     * @throws SQLException в случае возникновения при SQL запросе
      */
      public void updateUser(Integer id, String name, String login, String email) throws SQLException {
-         PreparedStatement pst = null;
-         try {
-             pst = conn.prepareStatement("UPDATE users SET name=?, login=?, email=? WHERE id=?");
+         try (PreparedStatement pst = this.conn.prepareStatement("UPDATE users SET name=?, login=?, email=? WHERE id=?")) {
              pst.setString(1, name);
              pst.setString(2, login);
              pst.setString(3, email);
              pst.setInt(4, id);
              pst.executeUpdate();
-
-         } finally {
-             if (pst !=  null) {
-                 pst.close();
-             }
          }
      }
 
     /**
      * Метод удаления пользователя из БД.
      * @param id записи в БД для удаления
-     * @throws SQLException
+     * @throws SQLException в случае возникновения при SQL запросе
      */
      public void deleteUser(Integer id) throws SQLException {
-         PreparedStatement pst = null;
-         try {
-             pst = conn.prepareStatement("DELETE FROM users WHERE id=?");
+         try (PreparedStatement pst = this.conn.prepareStatement("DELETE FROM users WHERE id=?")) {
              pst.setInt(1, id);
              pst.executeUpdate();
-
-         } finally {
-             if (pst != null) {
-                 pst.close();
-             }
          }
      }
 }
