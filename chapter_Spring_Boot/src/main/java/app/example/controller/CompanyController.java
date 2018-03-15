6776a6b927e6c59a04b38cd9d6b75e73c6e745db
mtskanton <mtsk.anton@yandex.ru>
2018-03-15 21:39:48 +0300

    REST controller

--- chapter_Spring_Boot/src/main/java/app/example/controller/CompanyController.java
+++ chapter_Spring_Boot/src/main/java/app/example/controller/CompanyController.java
@@ -1,13 +1,49 @@
 package app.example.controller;
 
+import app.example.model.Company;
 import app.example.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class CompanyController {
 
     @Autowired
     CompanyRepository companies;
 
+    // /companies
+    // /companies.xml
+    // /companies.json
+    @GetMapping(value = "/companies", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @ResponseBody
+    public List<Company> getCompanies() {
+        return (List<Company>) companies.findAll();
+    }
+
+    @GetMapping(value = "/company/{id}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    @ResponseBody
+    public Company getCompanyById(@PathVariable("id") String id) {
+        return companies.findOne(Integer.valueOf(id));
+    }
+
+    //Create
+    @PostMapping(value = "/company", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public void create(@RequestBody Company company) {
+        companies.save(company);
+    }
+
+    //Update
+    @PutMapping(value = "/company", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public void update(@RequestBody Company company) {
+        companies.save(company);
+    }
+
+    //Delete
+    @DeleteMapping(value = "/company/{id}")
+    public void delete(@PathVariable("id") String id) {
+        companies.delete(Integer.valueOf(id));
+    }
 }
